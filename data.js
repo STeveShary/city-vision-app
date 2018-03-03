484dd9c6230c889c23b924dd58e704efa80a0763
STeve Shary <stephen.shary@kroger.com>
2018-03-03 17:53:30 -0500

    Adding march dates

--- data.js
+++ data.js
@@ -7,72 +7,6 @@ export const getSchedule = () => {
 
 const schedule = [
     {
-      name: 'Home Church',
-      locationName: 'Schroedary House',
-      locationAddress: '21 Woodward Ave.  Buffalo, NY 14214',
-      timeStart: moment('2018-01-23T18:30:00-05:00')
-    },
-    {
-      name: 'Group Prayer',
-      locationName: 'The Birdhouse',
-      locationAddress: '419 Bird Ave Buffalo, NY 14213',
-      timeStart: moment('2018-01-27T08:00:00-05:00')
-    },
-    {
-      name: 'CT (Central Teaching)',
-      locationName: 'Rennovation Center (Upper Level)',
-      locationAddress: '567 Hertel Ave, Buffalo, NY 14207',
-      timeStart: moment('2018-01-28T17:15:00-05:00')
-    },
-    {
-      name: 'Group Prayer',
-      locationName: 'The Birdhouse',
-      locationAddress: '149 Bird Ave Buffalo, NY 14213',
-      timeStart: moment('2018-02-03T08:00:00-05:00')
-    },
-    {
-      name: 'Home Church (Teaching @ 5:15 with Superbowl Party after at 6:30)',
-      locationName: 'Schroedary House',
-      locationAddress: '21 Woodward Ave.  Buffalo, NY 14214',
-      timeStart: moment('2018-02-04T17:15:00-05:00')
-    },
-    {
-      name: 'Group Prayer',
-      locationName: 'The Birdhouse',
-      locationAddress: '149 Bird Ave Buffalo, NY 14213',
-      timeStart: moment('2018-02-10T08:00:00-05:00')
-    },
-    {
-      name: 'CT (Central Teaching)',
-      locationName: 'Rennovation Center (Upper Level)',
-      locationAddress: '567 Hertel Ave, Buffalo, NY 14207',
-      timeStart: moment('2018-02-11T17:15:00-05:00')
-    },
-    {
-      name: 'Group Prayer',
-      locationName: 'The Birdhouse',
-      locationAddress: '149 Bird Ave Buffalo, NY 14213',
-      timeStart: moment('2018-02-17T08:00:00-05:00')
-    },
-    {
-      name: 'Home Church',
-      locationName: 'Schroedary House',
-      locationAddress: '21 Woodward Ave.  Buffalo, NY 14214',
-      timeStart: moment('2018-02-18T17:15:00-05:00')
-    },
-    {
-      name: 'Group Prayer',
-      locationName: 'The Birdhouse',
-      locationAddress: '149 Bird Ave Buffalo, NY 14213',
-      timeStart: moment('2018-02-24T08:00:00-05:00')
-    },
-    {
-      name: 'CT (Central Teaching)',
-      locationName: 'Rennovation Center (Upper Level)',
-      locationAddress: '567 Hertel Ave, Buffalo, NY 14207',
-      timeStart: moment('2018-02-25T17:15:00-05:00')
-    },
-    {
       name: 'Group Prayer',
       locationName: 'The Birdhouse',
       locationAddress: '149 Bird Ave Buffalo, NY 14213',
@@ -83,5 +17,83 @@ const schedule = [
       locationName: 'Schroedary House',
       locationAddress: '21 Woodward Ave.  Buffalo, NY 14214',
       timeStart: moment('2018-03-04T17:15:00-05:00')
+    },
+    {
+      name: 'Group Prayer',
+      locationName: 'The Birdhouse',
+      locationAddress: '149 Bird Ave Buffalo, NY 14213',
+      timeStart: moment('2018-03-10T08:00:00-05:00')
+    },
+    {
+      name: 'CT (Central Teaching)',
+      locationName: 'Rennovation Center (Upper Level)',
+      locationAddress: '567 Hertel Ave, Buffalo, NY 14207',
+      timeStart: moment('2018-03-11T17:15:00-05:00')
+    },
+    {
+      name: 'Group Prayer',
+      locationName: 'The Birdhouse',
+      locationAddress: '149 Bird Ave Buffalo, NY 14213',
+      timeStart: moment('2018-03-17T08:00:00-05:00')
+    },
+    {
+      name: 'Home Church',
+      locationName: 'Schroedary House',
+      locationAddress: '21 Woodward Ave.  Buffalo, NY 14214',
+      timeStart: moment('2018-03-18T17:15:00-05:00')
+    },
+    {
+      name: 'Group Prayer',
+      locationName: 'The Birdhouse',
+      locationAddress: '149 Bird Ave Buffalo, NY 14213',
+      timeStart: moment('2018-03-24T08:00:00-05:00')
+    },
+    {
+      name: 'CT (Central Teaching)',
+      locationName: 'Rennovation Center (Upper Level)',
+      locationAddress: '567 Hertel Ave, Buffalo, NY 14207',
+      timeStart: moment('2018-03-25T17:15:00-05:00')
+    },
+    {
+      name: 'Group Prayer',
+      locationName: 'The Birdhouse',
+      locationAddress: '149 Bird Ave Buffalo, NY 14213',
+      timeStart: moment('2018-03-31T08:00:00-05:00')
+    },
+    {
+      name: 'Home Church',
+      locationName: 'Schroedary House',
+      locationAddress: '21 Woodward Ave.  Buffalo, NY 14214',
+      timeStart: moment('2018-04-01T17:15:00-05:00')
+    },
+    {
+      name: 'Group Prayer',
+      locationName: 'The Birdhouse',
+      locationAddress: '149 Bird Ave Buffalo, NY 14213',
+      timeStart: moment('2018-04-07T08:00:00-05:00')
+    },
+    {
+      name: 'CT (Central Teaching)',
+      locationName: 'Rennovation Center (Upper Level)',
+      locationAddress: '567 Hertel Ave, Buffalo, NY 14207',
+      timeStart: moment('2018-04-08T17:15:00-05:00')
+    },
+    {
+      name: 'Group Prayer',
+      locationName: 'The Birdhouse',
+      locationAddress: '149 Bird Ave Buffalo, NY 14213',
+      timeStart: moment('2018-04-14T08:00:00-05:00')
+    },
+    {
+      name: 'Home Church',
+      locationName: 'Schroedary House',
+      locationAddress: '21 Woodward Ave.  Buffalo, NY 14214',
+      timeStart: moment('2018-04-15T17:15:00-05:00')
+    },
+    {
+      name: 'Group Prayer',
+      locationName: 'The Birdhouse',
+      locationAddress: '149 Bird Ave Buffalo, NY 14213',
+      timeStart: moment('2018-04-21T08:00:00-05:00')
     }
   ];
