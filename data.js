697ff02ff839742da95a5cc42b479b533471ba4e
STeve Shary <stephen.shary@kroger.com>
2018-03-26 18:07:31 -0400

    Change in event location

--- data.js
+++ data.js
@@ -7,27 +7,9 @@ export const getSchedule = () => {
 
 const schedule = [
     {
-      name: 'Home Church',
+      name: 'Group Prayer (This Week)',
       locationName: 'Schroedary House',
       locationAddress: '21 Woodward Ave.  Buffalo, NY 14214',
-      timeStart: moment('2018-03-18T17:15:00-04:00')
-    },
-    {
-      name: 'Group Prayer',
-      locationName: 'The Birdhouse',
-      locationAddress: '149 Bird Ave Buffalo, NY 14213',
-      timeStart: moment('2018-03-24T08:00:00-04:00')
-    },
-    {
-      name: 'CT (Central Teaching)',
-      locationName: 'Rennovation Center (Upper Level)',
-      locationAddress: '597 Hertel Ave, Buffalo, NY 14207',
-      timeStart: moment('2018-03-25T17:15:00-04:00')
-    },
-    {
-      name: 'Group Prayer',
-      locationName: 'The Birdhouse',
-      locationAddress: '149 Bird Ave Buffalo, NY 14213',
       timeStart: moment('2018-03-31T08:00:00-04:00')
     },
     {
